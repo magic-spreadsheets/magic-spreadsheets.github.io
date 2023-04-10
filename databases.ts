5106f67e3734ee0c0281f8bcff3c6b3d707902f0
keigomichi <hundredacrewood1165g7@gmail.com>
2023-04-10 16:51:12 +0900

    feat: add databases and maintainers info

---
+++ databases.ts
@@ -0,0 +1,40 @@
+import type { Databases } from './src/types';
+
+export const databases: Databases = {
+  2023: {
+    text: '魔法のスプレッドシート2023・夏',
+    href: 'https://deep-professor-e3c.notion.site/049ca5329bbc4493bc9f4f5b3727d027?v=3b000340195242b0bd4b0741063ec259',
+  },
+  2022: {
+    text: '2022夏ITエンジニアインターン',
+    href: 'https://docs.google.com/spreadsheets/d/1O4LqD6tfVisshD4YngjEKwpU1FQCGuD8vZ1qlVX0sBY/edit#gid=762111034',
+  },
+  2021: {
+    text: '2021夏ITエンジニアインターン',
+    href: 'https://docs.google.com/spreadsheets/d/1O4LqD6tfVisshD4YngjEKwpU1FQCGuD8vZ1qlVX0sBY/edit#gid=62946583',
+  },
+  2020: {
+    text: '2020夏ITエンジニアインターン',
+    href: 'https://docs.google.com/spreadsheets/d/1O4LqD6tfVisshD4YngjEKwpU1FQCGuD8vZ1qlVX0sBY/edit#gid=1644405096',
+  },
+  2019: {
+    text: '2019夏ITエンジニアインターン',
+    href: 'https://docs.google.com/spreadsheets/d/1O4LqD6tfVisshD4YngjEKwpU1FQCGuD8vZ1qlVX0sBY/edit#gid=0',
+  },
+  2018: {
+    text: '2018夏ITエンジニアインターン',
+    href: 'https://docs.google.com/spreadsheets/d/1O4LqD6tfVisshD4YngjEKwpU1FQCGuD8vZ1qlVX0sBY/edit#gid=1234947860',
+  },
+  2017: {
+    text: '2017夏ITエンジニアインターン',
+    href: 'https://docs.google.com/spreadsheets/d/1O4LqD6tfVisshD4YngjEKwpU1FQCGuD8vZ1qlVX0sBY/edit#gid=211039304',
+  },
+  2016: {
+    text: '2016夏ITエンジニアインターン',
+    href: 'https://docs.google.com/spreadsheets/d/1O4LqD6tfVisshD4YngjEKwpU1FQCGuD8vZ1qlVX0sBY/edit#gid=397387909',
+  },
+  2015: {
+    text: '2015夏ITエンジニアインターン',
+    href: 'https://docs.google.com/spreadsheets/d/1O4LqD6tfVisshD4YngjEKwpU1FQCGuD8vZ1qlVX0sBY/edit#gid=142019139',
+  },
+};
---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export type Maintainers = Record<number, Maintainer[]>;
+
+export type Maintainer = {
+  text: string;
+  href?: string;
+};
+
+export type Databases = Record<number, Database>;
+
+export type Database = {
+  text: string;
+  href: string;
+};
