71bfb4ba7ea8df1efa43019366793e6dac7ab257
keigomichi <hundredacrewood1165g7@gmail.com>
2023-04-10 23:42:28 +0900

    chore: update maintainers

--- maintainers.ts
+++ maintainers.ts
@@ -15,32 +15,4 @@ export const maintainers: Maintainers = {
       href: 'https://twitter.com/chnotchy',
     },
   ],
-  2022: [
-    {
-      text: '@shota_kizawa',
-      href: 'https://twitter.com/shota_kizawa',
-    },
-  ],
-  2021: [
-    {
-      text: '@hpp_ricecake',
-      href: 'https://twitter.com/hpp_ricecake',
-    },
-    {
-      text: '@kaito_tateyama',
-      href: 'https://twitter.com/kaito_tateyama',
-    },
-  ],
-  2020: [
-    {
-      text: '@Shinyaigeek',
-      href: 'https://twitter.com/Shinyaigeek',
-    },
-  ],
-  2019: [
-    {
-      text: '@p1ass',
-      href: 'https://twitter.com/p1ass',
-    },
-  ],
 };
