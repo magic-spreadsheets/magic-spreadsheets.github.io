d6189791b7b43b08e232f1267504073fff44d5c6
mshr0969 <mshr60531@gmail.com>
2024-10-16 00:37:20 +0900

    feat: 🎉 update adimin to mshr0969

--- community-info.ts
+++ community-info.ts
@@ -2,6 +2,6 @@ import type { ContactDetails } from './src/types';
 
 // お問い合わせ先
 export const contactDetails: ContactDetails = {
-  text: '@mikan_54951',
-  href: 'https://twitter.com/mikan_54951',
+  text: '@mshr0969',
+  href: 'https://twitter.com/msr0969',
 };
