128653efc762f4ba0eab9d8789d9b944ee8e055a
mshr0969 <mshr60531@gmail.com>
2024-10-16 01:35:09 +0900

    fix: 🔧 update username

--- community-info.ts
+++ community-info.ts
@@ -3,5 +3,5 @@ import type { ContactDetails } from './src/types';
 // お問い合わせ先
 export const contactDetails: ContactDetails = {
   text: '@mshr0969',
-  href: 'https://twitter.com/msr0969',
+  href: 'https://twitter.com/mshr0969',
 };
