ea1408f671041551f36431b458ddb389c1ca463e
BuildTools <unconfigured@null.spigotmc.org>
2026-02-09 16:46:17 +0900

    chore: change admin to yusatck

--- community-info.ts
+++ community-info.ts
@@ -2,6 +2,6 @@ import type { ContactDetails } from './src/types';
 
 // お問い合わせ先
 export const contactDetails: ContactDetails = {
-  text: '@mshr0969',
-  href: 'https://twitter.com/mshr0969',
+  text: '@yusatck',
+  href: 'https://twitter.com/yusatck',
 };
